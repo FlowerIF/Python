21e7f204b86c4c25c684f2b29438df5c16c26a5b
FlowerIF <dotv182@gmail.com>
2025-02-10 22:21:58 +0700

    add if else

---
+++ Day_3/if_else.py
@@ -0,0 +1,8 @@
+num = int(input("enter number: "))
+
+if num  == 0:
+    print('ban da nhap vao so 0')
+elif num % 2 == 0:
+    print('ban da nhap vao so chan')
+else:
+    print('ban da nhap vao so le')
