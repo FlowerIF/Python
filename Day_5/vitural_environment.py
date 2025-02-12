1ed8986c46579b3c09a1c6707fab9923b12de71d
FlowerIF <dotv182@gmail.com>
2025-02-12 22:36:23 +0700

    add environ

---
+++ Day_5/vitural_environment.py
@@ -0,0 +1,6 @@
+# lý do cần sử dụng môi trường ảo, dễ quản lý các gói của mình, tránh xung đột giữa các phiên bản packet
+# tạo môi trường ảo
+#python -m venv tên
+#.\tên\Scripts\activate để active môi trường ảo
+
+
